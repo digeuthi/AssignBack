90e80740696250178ec959bfe0dffdec103b2213
Dahye Kim <digeuthi@gmail.com>
2023-07-13 23:12:22 +0900

    jwt problem

--- src/main/java/com/example/board/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/board/filter/JwtAuthenticationFilter.java
@@ -36,12 +36,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
 
                 try{
                     String jwt = parseToken(request);
-
-                    boolean hasJwt = jwt != null;
-                    if(!hasJwt) {
+                    
+                    // 토큰이 null이거나 비어 있는지 확인합니다.
+                    if (jwt == null || jwt.isEmpty()) {
                         filterChain.doFilter(request, response);
                         return;
-                    }
+            }
+
+                    // 토큰이 "Bearer "로 시작하는 지 확인합니다.
+                    if (!jwt.startsWith("Bearer ")) {
+                    return;
+            }
+
+                    // 토큰에서 "Bearer " 접두사를 제거합니다.
+                    jwt = jwt.substring(7);
+
+                    // boolean hasJwt = jwt != null;
+                    // if(!hasJwt) {
+                    //     filterChain.doFilter(request, response);
+                    //     return;
+                    // }
                     String email = jwtProvider.validate(jwt);
 
                     boolean comparedResult = userService.validateStoredToken(email, jwt);
@@ -73,13 +87,23 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
 
         String token = request.getHeader("Authorization");
         
-        boolean hasToken = 
-            token != null && 
-            !token.equalsIgnoreCase("null");
-        if(!hasToken) return null;
+        // 토큰이 null이거나 비어 있는지 확인합니다.
+        if (token == null || token.isEmpty()) {
+            return null;
+        }
 
-        boolean isbearer = token.startsWith("Bearer ");
-        if(!isbearer) return null;
+        // 토큰이 "Bearer "로 시작하는 지 확인합니다.
+        if (!token.startsWith("Bearer ")) {
+            return null;
+        }
+        
+        // boolean hasToken = 
+        //     token != null && 
+        //     !token.equalsIgnoreCase("null");
+        // if(!hasToken) return null;
+
+        // boolean isbearer = token.startsWith("Bearer ");
+        // if(!isbearer) return null;
 
         String jwt = token.substring(7);
         return jwt;
